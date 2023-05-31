13ad788092a45dd623b44378994fdd2a7e6f119a
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 00:30:53 +0530

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -1,38 +1,38 @@
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "vpc1" {
   cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.vpc1.id
   cidr_block              = var.subnet_cidr_block
   availability_zone       = var.availability_zone
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_internet_gateway" "igw1" {
+  vpc_id = aws_vpc.vpc1.id
 }
 
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_route_table" "routetable1" {
+  vpc_id = aws_vpc.vpc1.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.igw1.id
   }
 }
 
 output "vpc_id" {
-  value = aws_vpc.my_vpc.id
+  value = aws_vpc.vpc1.id
 }
 
 output "subnet_id" {
-  value = aws_subnet.my_subnet.id
+  value = aws_subnet.subnet1.id
 }
 
 output "route_table_id" {
-  value = aws_route_table.my_route_table.id
+  value = aws_route_table.routetable1.id
 }
 
 output "igw_id" {
-  value = aws_internet_gateway.my_igw.id
+  value = aws_internet_gateway.igw1.id
 }
