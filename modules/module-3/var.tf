62326d1350eda957f27ac26b93260386676d9008
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-05-31 23:11:27 +0530

    Create var.tf

---
+++ modules/module-3/var.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  description = "AMI ID of the instance"
+}
+
+variable "instance_type" {
+  description = "Instance type"
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+}
+
+variable "security_group_id" {
+  description = "Security Group ID"
+}
