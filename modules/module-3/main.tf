5118f40f7aaf6ec092785e0ce55c05fc8c20d066
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 00:36:11 +0530

    Update main.tf

--- modules/module-3/main.tf
+++ modules/module-3/main.tf
@@ -1,18 +1,18 @@
-resource "aws_instance" "instance1" {
+resource "aws_instance" "master" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   security_group_ids = [var.security_group_id]
 }
 
-resource "aws_instance" "instance2" {
+resource "aws_instance" "worker_1" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   security_group_ids = [var.security_group_id]
 }
 
-resource "aws_instance" "instance3" {
+resource "aws_instance" "worker_2" {
   ami           = var.ami
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
