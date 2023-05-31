9ccb1b0e6f86d0b5e5fc3c364344be7c0b472667
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-05-31 23:05:27 +0530

    Create main.tf

---
+++ modules/module-2/main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "allow_all" {
+  name        = var.security_group_name
+  description = "Allow all traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
