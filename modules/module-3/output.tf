22d8cde3b5547dfb6751a81d6a1c83125fa78e17
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-05-31 23:12:04 +0530

    Create output.tf

---
+++ modules/module-3/output.tf
@@ -0,0 +1,11 @@
+output "instance1_id" {
+  value = aws_instance.instance1.id
+}
+
+output "instance2_id" {
+  value = aws_instance.instance2.id
+}
+
+output "instance3_id" {
+  value = aws_instance.instance3.id
+}
