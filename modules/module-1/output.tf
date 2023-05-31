ca99b860f5126e26a9e648e9bee49d567544eb76
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 00:32:03 +0530

    Update output.tf

--- modules/module-1/output.tf
+++ modules/module-1/output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
   description = "ID of the created VPC"
-  value       = aws_vpc.my_vpc.id
+  value       = aws_vpc.vpc1.id
 }
 
 output "subnet_id" {
   description = "ID of the created subnet"
-  value       = aws_subnet.my_subnet.id
+  value       = aws_subnet.subnet1.id
 }
 
 output "route_table_id" {
   description = "ID of the created route table"
-  value       = aws_route_table.my_route_table.id
+  value       = aws_route_table.routetable1.id
 }
 
 output "igw_id" {
   description = "ID of the created internet gateway"
-  value       = aws_internet_gateway.my_igw.id
+  value       = aws_internet_gateway.igw1.id
 }
