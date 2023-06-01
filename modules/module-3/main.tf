62b3ba51393c8f22770645921afbe6fee4fe538b
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 11:48:56 +0530

    Update main.tf

--- modules/module-3/main.tf
+++ modules/module-3/main.tf
@@ -1,20 +1,26 @@
 resource "aws_instance" "master" {
+  instance_name = "master"
   ami           = var.ami
-  instance_type = var.instance_type
+  key           = "tokyo"
+  instance1_type = var.instance_type
   subnet_id     = var.subnet_id
   security_group_ids = var.security_group_id
 }
 
 resource "aws_instance" "worker_1" {
+  instance_name = "worker_1"
   ami           = var.ami
-  instance_type = var.instance_type
+  key           = "tokyo"
+  instance2_type = var.instance_type
   subnet_id     = var.subnet_id
   security_group_ids = var.security_group_id
 }
 
 resource "aws_instance" "worker_2" {
+  instance_nmae = "worker_2"
   ami           = var.ami
-  instance_type = var.instance_type
+   key           = "tokyo"
+  instance3_type = var.instance_type
   subnet_id     = var.subnet_id
   security_group_ids = var.security_group_id
 }
