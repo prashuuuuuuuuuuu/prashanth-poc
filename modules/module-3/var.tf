31df7a84d3d50d8574c1c2f11958266d193376a9
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 09:31:41 +0530

    Update var.tf

--- modules/module-3/var.tf
+++ modules/module-3/var.tf
@@ -1,15 +1,19 @@
 variable "ami" {
-  description = "ami-0ed99df77a82560e6"
+  description = "ami"
+  value = ami-0ed99df77a82560e6
 }
 
 variable "instance_type" {
-  description = "t2.medium"
+  description = "instance_type"
+  value = t2.medium
 }
 
 variable "subnet_id" {
-  description = "aws_subnet.subnet1.id"
+  description = "aws_subnet_id"
+  default = aws_subnet.subnet1.id
 }
 
 variable "security_group_id" {
-  description = "aws_security_group.security_group1.id"
+  description = "aws_security_group_id"
+  value = aws_security_group.security_group1.id
 }
