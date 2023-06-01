a6fce70c6bb2ba9f844ea35b855b93639c8f062e
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 10:24:09 +0530

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -1,38 +1,45 @@
-resource "aws_vpc" "vpc1" {
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "my_vpc" {
   cidr_block = var.vpc_cidr_block
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id                  = aws_vpc.vpc1.id
-  cidr_block              = var.subnet_cidr_block
-  availability_zone       = var.availability_zone
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.subnet_availability_zone
 }
 
-resource "aws_internet_gateway" "igw1" {
-  vpc_id = aws_vpc.vpc1.id
-}
-
-resource "aws_route_table" "routetable1" {
-  vpc_id = aws_vpc.vpc1.id
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw1.id
+    gateway_id = aws_internet_gateway.my_igw.id
   }
 }
 
-output "vpc_id" {
-  value = aws_vpc.vpc1.id
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
 }
 
-output "subnet_id" {
-  value = aws_subnet.subnet1.id
-}
+resource "aws_security_group" "my_security_group" {
+  name        = "allow-all-traffic"
+  description = "Security group allowing all traffic"
 
-output "route_table_id" {
-  value = aws_route_table.routetable1.id
-}
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-output "igw_id" {
-  value = aws_internet_gateway.igw1.id
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
