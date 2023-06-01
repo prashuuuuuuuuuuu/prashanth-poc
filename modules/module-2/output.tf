efd8ad998ad27202b7227c914e16693950e90708
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 07:59:15 +0530

    Update output.tf

--- modules/module-2/output.tf
+++ modules/module-2/output.tf
@@ -1,4 +1,4 @@
 output "security_group_id" {
   description = "ID of the created security group"
-  value       = aws_security_group.allow_all.id
+  value       = aws_security_group.security_group_1.id
 }
