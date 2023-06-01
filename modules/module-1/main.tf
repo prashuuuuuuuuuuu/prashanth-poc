e908f0b7085169fdc89154af41dc5f4a911b2a3d
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 11:38:29 +0530

    Update main.tf

--- modules/module-1/main.tf
+++ modules/module-1/main.tf
@@ -58,7 +58,14 @@ output "subnet_id" {
   value       = aws_subnet.subnet1.id
 }
 
-output "security_group_id"
+output "security_group_id" {
   description = "ID of the security_group_id"
   default =  aws_security_group.security_group1.id"
+}
+
+output "route_table_id" {
+  description = "ID of created route_table"
+  value = aws_route_table.route_table1.id
+  }
+
 
