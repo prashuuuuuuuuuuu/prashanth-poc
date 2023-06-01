102199cfaa06549d2a55d478ddc67346cf902a49
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 08:02:22 +0530

    Update output.tf

--- modules/module-3/output.tf
+++ modules/module-3/output.tf
@@ -1,11 +1,11 @@
 output "instance1_id" {
-  value = aws_instance.instance1.id
+  value = "aws_instance.instance1.id"
 }
 
 output "instance2_id" {
-  value = aws_instance.instance2.id
+  value = "aws_instance.instance2.id"
 }
 
 output "instance3_id" {
-  value = aws_instance.instance3.id
+  value = "aws_instance.instance3.id"
 }
