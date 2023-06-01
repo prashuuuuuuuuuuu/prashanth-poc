c35603d6e4f1e2b14f2d4ca7b9ea139e0cf740a5
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-01 09:42:58 +0530

    Update output.tf

--- modules/module-3/output.tf
+++ modules/module-3/output.tf
@@ -9,3 +9,9 @@ output "instance2_id" {
 output "instance3_id" {
   value = "aws_instance.instance3.id"
 }
+
+output "subnet_id" {
+  value = "aws_subnet_id.subnet1.id"
+  
+  output "vpc_id" {
+    value = "aws_vpc_id.vpc1.id"
