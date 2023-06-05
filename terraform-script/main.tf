7f413cfff2aeb8b30faf9c6a5ce5d89ef8fabc4d
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-05 21:00:40 +0530

    Update main.tf

--- terraform-script/main.tf
+++ terraform-script/main.tf
@@ -1,5 +1,15 @@
+terraform {
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = "~> 4.0"
+}
+}
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region = var.region
+region = "ap-northeast-2"
 }
 
 # Create a VPC
