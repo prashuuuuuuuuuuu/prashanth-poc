fa974b54750d9626dc1e9fc3a44f73f8b7526b65
prashuuuuuuuuuuu <91795639+prashuuuuuuuuuuu@users.noreply.github.com>
2023-06-05 20:58:49 +0530

    Update variables.tf

--- terraform-script/variables.tf
+++ terraform-script/variables.tf
@@ -1,26 +1,26 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-2"
+  default     = "ap-northeast-1"
 }
 
 variable "vpc_cidr_block" {
   description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  default     = "30.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
   description = "CIDR block for the subnet"
-  default     = "10.0.1.0/24"
+  default     = "30.0.1.0/24"
 }
 
 variable "subnet_availability_zone" {
   description = "Availability zone for the subnet"
-  default     = "us-west-2a"
+  default     = "ap-northeast-1a"
 }
 
 variable "ami_id" {
   description = "AMI ID for the instances"
-  default     = "ami-12345678"
+  default     = "ami-0ed99df77a82560e6"
 }
 
 variable "instance_type" {
@@ -29,5 +29,6 @@ variable "instance_type" {
 }
 
 variable "peer_vpc_id" {
-  description = "ID of the peer VPC for VPC peering"
+  description = "vpc-06fa15513cc3b550b"
+  value = "vpc-06fa15513cc3b550b"
 }
